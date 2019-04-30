6e45771e4a1ad87350865d5921c47d4e734e991d
Q.McKinley <q.mckinley@outlook.com>
2019-04-29 20:49:12 -0400

    add sec_23

---
+++ Zero_To_Master-Udemy/Section_23/tachyons.js
@@ -0,0 +1,34 @@
+// tachyon cheatsheet
+
+className=''
+
+w-30 // width 30%
+
+grow // grow on hover
+
+f4 // font-size
+
+link //
+
+ph3 //
+
+pv2 //
+
+pa2 // padding
+
+dib 
+
+white // text color
+
+bg-light-purple // background color
+
+br3 // border radius
+
+shadow-5 // shadow
+
+pointer // pointer on hover
+
+ma2 // margin all
+
+mt // margin top
+
