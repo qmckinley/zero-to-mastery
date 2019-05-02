c1b79540fa42026a92e94d2b3db2a61d0b8c2a72
Q.McKinley <q.mckinley@outlook.com>
2019-05-02 16:52:42 -0400

    add node sec24

---
+++ Zero_To_Master-Udemy/Section_24/node/script.js
@@ -0,0 +1,19 @@
+const c = require('./script2.js')
+
+const a = c.largeNumber;
+const b = 6;
+
+console.log(a + b);
+
+//
+
+// const d = require('fs');
+
+// console.log(d);
+
+//
+
+// const e = require('http');
+
+//
+
---
+++ Zero_To_Master-Udemy/Section_24/node/server5.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/:id', (req, res) => {
+  // console.log(req.query);
+  // req.body
+  // console.log(req.header);
+  console.log(req.params);
+  res.status(404).send('not found');
+});
+
+app.listen(3000);
